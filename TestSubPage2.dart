14d5ca4257da06ddc9b8d988e12ad80ab1f2fe0f
coleforeman <cforeman876@gmail.com>
2022-07-05 23:35:13 -0600

    Update TestSubPage2.dart

--- TestSubPage2.dart
+++ TestSubPage2.dart
@@ -1,12 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ItemPageDetailView2 extends StatelessWidget {
   final DocumentSnapshot itModel;
 
   const ItemPageDetailView2({
-    Key itkey,
-    @required this.itModel,
+    Key? itkey,
+    required this.itModel,
   }) : super(key: itkey);
 
   Widget build(BuildContext context) {
@@ -24,8 +24,7 @@ class ItemPageDetailView2 extends StatelessWidget {
         body: Container(
           decoration: BoxDecoration(
               image: DecorationImage(
-                  image: NetworkImage(itModel.get('*Insert Name of Firestore Document Field With Firebase Storage Photo Downloadurl*')),
-                  fit: BoxFit.cover)),
+                  image: NetworkImage(itModel.get('')), fit: BoxFit.cover)),
         ));
   }
 }
